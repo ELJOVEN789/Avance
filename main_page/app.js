088d78bd0e46b28a1d2711f218f4efe55cb44219
Sebastian <sebases282005@gmail.com>
2022-11-17 00:30:24 -0500

    Avances

--- main_page/app.js
+++ main_page/app.js
@@ -20,3 +20,35 @@ oscuro.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
     oscuro.classList.toggle('active');
 });
+
+//ANIMACION TITULOS
+
+const watch = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('look');
+        }else {
+            entry.target.classList.remove('look');
+        }
+    });
+});
+
+const title = document.querySelectorAll('.section__animation');
+title.forEach((el) => watch.observe(el));
+
+//ANIMACION AGRADECIMIENTOS
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const elements = document.querySelectorAll('.thanks');
+elements.forEach((el) => observer.observe(el));
